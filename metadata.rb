4cbee89da4fea8aaba28e755d54a6da6ba04f604
Andrew Bruce <andrew@camelpunch.com>
2011-02-16 01:37:15 +0000

    update timezone metadata

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,6 @@
-maintainer       "YOUR_COMPANY_NAME"
-maintainer_email "YOUR_EMAIL"
-license          "All rights reserved"
+maintainer       "Camel Punch"
+maintainer_email "admin@camelpunch.com"
+license          "BSD"
 description      "Installs/Configures timezone"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
 version          "0.0.1"
